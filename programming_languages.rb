0b82ab7c8cd4c6105c8b49f0d30c15100726a7f7
Miles Marillo <mjm03@hampshire.edu>
2019-04-09 18:50:35 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,8 @@ def reformat_languages(languages)
 new_hash = {}
 languages.each do |style, data|
   data.each do |lang_name, describe|
-    
+    if new_hash[lang_name] == nil
+       new_hash[lang_name] = describe
 
 
 
